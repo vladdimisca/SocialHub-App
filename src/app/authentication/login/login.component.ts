4f63f74bcf5f02958a61921bf2dc319668eab189
vladdimisca <vladdimisca@yahoo.com>
2020-07-29 21:43:00 +0300

    small changes

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -27,7 +27,10 @@ export class LoginComponent {
         };
 
         this.authenticationService.login(user).subscribe(
-            (token) => { this.resetFields(); this.router.navigate(['/home']); }, // to be continued ...
+            (token) => {
+                this.resetFields();
+                this.router.navigate(['/home']);
+            }, // to be continued ...
             (err) => { console.log(err); }
         );
     }
