49f02ea1ba650afb4a9a27d74e43e3fcbdc09998
vladdimisca <vladdimisca@yahoo.com>
2020-08-02 22:27:55 +0300

    adjustments

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -8,7 +8,6 @@ import { NavbarModule } from '../navbar/navbar.module';
 // components
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
-
 // services
 import { GlobalService } from '../utils/global.service';
 
